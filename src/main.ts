156b0b31015d6e2594f85cb96c419db7d66aee03
hackster <m.hackstero@gmailcom>
2023-10-13 15:41:21 +0300

    Firebase database initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import * as serviceAccount from '../token/nest102023-firebase-adminsdk-839nr-b47c046314.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+
+const db = admin.firestore();
