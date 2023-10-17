69aabe37d705a765777abb756ebf371e35f6670f
hackster <m.hackstero@gmailcom>
2023-10-18 02:31:09 +0300

    added swagger.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -9,11 +11,19 @@ async function bootstrap() {
     .setTitle('API документация')
     .setDescription('Описание API')
     .setVersion('1.0')
+    .addTag('API')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
+  const yamlDocument = yaml.dump(document);
+  fs.writeFileSync('./swagger.yaml', yamlDocument, 'utf8');
+
   SwaggerModule.setup('api', app, document);
+
   const port = 3000;
+
+  console.log('API: http://localhost:3000/api');
 
   await app.listen(port, () => {
     console.log(`Application is running on http://localhost:${port}`);
